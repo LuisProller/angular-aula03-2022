6139656c5aa329e810c82393bc20245d049b9946
Luis Dutra <luisfgdutra@gmail.com>
2022-05-01 18:58:08 +0000

    App module com pipes nativos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import localePt from '@angular/common/locales/pt';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConsultaSaldoComponent } from './components/consulta-saldo/consulta-saldo.component';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt, 'pt-BR');
 
 @NgModule({
   declarations: [
     AppComponent,
+    ConsultaSaldoComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
   ],
   providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ],
   bootstrap: [
     AppComponent,
